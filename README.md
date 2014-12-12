b1ab875e0c3a1747f6380f67a1f0dd22ef1f4097
tyranron <tyranron@gmail.com>
2014-12-13 01:05:51 +0200

    added basic types

--- README.md
+++ README.md
@@ -4,14 +4,21 @@ Sizeof tip for Go types
 | Type | unsafe.Sizeof() bytes |
 | ---- | --------------------- |
 | `struct{}` | 0 |
+| `[0]Type` | 0 |
 | `bool` | 1 |
 | `int8`, `uint8`, `byte` | 1 |
 | `int16`, `uint16` | 2 |
 | `int32`, `uint32`, `rune` | 4 |
-| `int64`, `uint64` | 8 |
-| `int`, `uint` | 4 or 8 |
-| `uintptr` | 8 |
 | `float32` | 4 |
+| `int`, `uint` | 4 or 8 |
+| `int64`, `uint64` | 8 |
 | `float64` | 8 |
 | `complex64` | 8 |
+| `uintptr` | 8 |
+| `*struct{}`, `*int`, `*Type` | 8 |
+| `map[Type1]Type2` | 8 |
+| `chan Type` | 8 |
+| `func()` | 8 |
+| `string` | 16 |
 | `complex128` | 16 |
+| `[]Type` | 24 |
--- generate.go
+++ generate.go
@@ -13,17 +13,24 @@ type row struct {
 
 var Table = []row{
 	row{[]string{"struct{}"}, []uintptr{unsafe.Sizeof(struct{}{})}},
+	row{[]string{"[0]Type"}, []uintptr{unsafe.Sizeof([0]bool{})}},
 	row{[]string{"bool"}, []uintptr{unsafe.Sizeof(true)}},
 	row{[]string{"int8", "uint8", "byte"}, []uintptr{unsafe.Sizeof(byte(0))}},
 	row{[]string{"int16", "uint16"}, []uintptr{unsafe.Sizeof(int16(0))}},
 	row{[]string{"int32", "uint32", "rune"}, []uintptr{unsafe.Sizeof(rune(0))}},
-	row{[]string{"int64", "uint64"}, []uintptr{unsafe.Sizeof(int64(0))}},
-	row{[]string{"int", "uint"}, []uintptr{unsafe.Sizeof(int32(0)), unsafe.Sizeof(int64(0))}},
-	row{[]string{"uintptr"}, []uintptr{unsafe.Sizeof(uintptr(0))}},
 	row{[]string{"float32"}, []uintptr{unsafe.Sizeof(float32(0))}},
+	row{[]string{"int", "uint"}, []uintptr{unsafe.Sizeof(int32(0)), unsafe.Sizeof(int64(0))}},
+	row{[]string{"int64", "uint64"}, []uintptr{unsafe.Sizeof(int64(0))}},
 	row{[]string{"float64"}, []uintptr{unsafe.Sizeof(float64(0))}},
 	row{[]string{"complex64"}, []uintptr{unsafe.Sizeof(complex(float32(0), float32(0)))}},
+	row{[]string{"uintptr"}, []uintptr{unsafe.Sizeof(uintptr(0))}},
+	row{[]string{"*struct{}", "*int", "*Type"}, []uintptr{unsafe.Sizeof(&struct{}{})}},
+	row{[]string{"map[Type1]Type2"}, []uintptr{unsafe.Sizeof(map[string]bool{})}},
+	row{[]string{"chan Type"}, []uintptr{unsafe.Sizeof(make(chan bool))}},
+	row{[]string{"func()"}, []uintptr{unsafe.Sizeof(func(){})}},
+	row{[]string{"string"}, []uintptr{unsafe.Sizeof("")}},
 	row{[]string{"complex128"}, []uintptr{unsafe.Sizeof(complex(float64(0), float64(0)))}},
+	row{[]string{"[]Type"}, []uintptr{unsafe.Sizeof([]bool{})}},
 }
 
 func main() {
